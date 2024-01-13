6d0846cc2545b85e0aaf175426a366b01e7c63ae
sami5671 <samialam5671@gmail.com>
2024-01-13 22:47:06 +0600

    added the allPlants api

--- index.js
+++ index.js
@@ -2,7 +2,72 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+// Plant - World;
+// y8w9wjk07j8XUdTl;
+// =================================================================
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fmvmv30.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+
+    // =================All Collection================================================
+    const userCollection = client.db("PlantDB").collection("users");
+    const AllPlantsCollection = client.db("PlantDB").collection("AllPlants");
+    // =================================================================
+
+    // ---------------------All user ---------------------------------------
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "User already exists", insertedId: null });
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.get("/users", async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+    // =======================Plants Related Api==========================================
+    app.get("/AllPlants", async (req, res) => {
+      const result = await AllPlantsCollection.find().toArray();
+      res.send(result);
+    });
+    // =================================================================
+    // ------------------------------------------------------------
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 // =================================================================
 app.get("/", (req, res) => {
