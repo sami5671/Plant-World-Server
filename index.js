4214c9f2ffe942157f39366aef999bfd44531a4d
sami5671 <samialam5671@gmail.com>
2024-01-24 12:18:31 +0600

    added the trending product api

--- index.js
+++ index.js
@@ -136,6 +136,18 @@ async function run() {
       const result = await AllPlantsCollection.deleteOne(query);
       res.send(result);
     });
+
+    app.patch("/product/trending/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          trending: "true",
+        },
+      };
+      const result = await AllPlantsCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
     // =================================================================
 
     // =======================Cart Collection==========================================
