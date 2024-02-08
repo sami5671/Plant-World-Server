3886d04b7867a7297a840c71f7fe6748ae499bd7
sami5671 <samialam5671@gmail.com>
2024-02-08 23:57:01 +0600

    added the deletepaymentHistoy api

--- index.js
+++ index.js
@@ -346,6 +346,12 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/deletePaymentHistory/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await paymentHistoryCollection.deleteOne(query);
+      res.send(result);
+    });
     // -----------------------------------------------------------------
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
